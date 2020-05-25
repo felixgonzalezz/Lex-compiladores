be353a01d2443a3bebea07e5099eda1ce89d0385
Felix Gonzalez <fgz.gonz@gmail.com>
2020-05-24 21:52:19 -0500

    agrego proyecto

---
+++ Programa_1/tokens.h
@@ -0,0 +1,25 @@
+//En este programa definirimeos los terminales
+#ifndef TOKENS_H
+#define TOKENS_H
+
+//A continuacion definimos las cosntantes para cada simbolo terminal
+#define P_RESERVADA
+#define P_DEFINE
+#define P_RETURN
+#define OPERADOR
+#define P_INCREMENTO
+#define P_DECREMENTO
+#define A_LLAVE
+#define C_LLAVE
+#define A_PAR
+#define C_PAR
+#define A_CORCHETE
+#define C_CORCHETE
+#define FIN_SENTECIA
+#define T_BASE
+#define S_NUMERAL
+#define S_MAYOR
+#define S_MENOR
+#define S_COMA
+#define S_IGUAL
+#define LIBRERIAS
