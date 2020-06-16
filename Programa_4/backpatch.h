ccffd299256624b319e9903b14ef561ca398e58b
Simón Ramírez <sera.-@hotmail.com>
2020-06-15 21:41:23 -0500

    Programa 4, programas de Ulises

---
+++ Programa_4/backpatch.h
@@ -0,0 +1,28 @@
+#include "cuadruplas.h"
+
+typedef struct index INDEX;
+
+struct index{
+    char *indice ;
+    INDEX *next ;
+} ;
+
+typedef struct listindex{
+    INDEX *head;
+    INDEX *tail;
+} LINDEX;
+
+INDEX *init_index();// Reserva memoria para un nodo de indice
+
+void finish_index(INDEX *i); // Libera la memoria de un nodo indice
+
+LINDEX *init_list_index(INDEX *i); //Reserva memoria para la lista de indice se inserta el primero
+
+void *finish_list_index(CODE *c); //Libera la memoria de la lista y de todos los nodos dentro
+
+void append_index(LINDEX *l ,INDEX *i); // Agrega un nodo indice al final de la lista
+
+LINDEX *combinar (LINDEX l1, LINDEX l2); //Retorna una lista ligada de la concatenacion de l1 con l2
+
+void backpatch(CODE *c, LINDEX l, char *label); 
+//Reemplaza label en cada aparicion de un indice en la cuadruplas del codigo c
---
+++ Programa_4/cuadruplas.h
@@ -0,0 +1,23 @@
+typedef struct cuad CUAD;
+struct cuad {
+    char *op;
+    char *arg1;
+    char *arg2;
+    char *res;
+    CUAD *next;
+} ;
+
+typedef struct code{
+    CUAD *head ;
+    CUAD *tail;
+} CODE;
+
+CUAD *init_quad(); // Reserva memoria para una cuadrupla
+
+void finish_quad(CUAD *c); // Libera la memoria de una cuadrupla
+
+CODE *init_code(); // Reserva memoria para el codigo
+
+void *finish_code(CODE *c); //Libera la memoria de la lista ligada del codigo
+
+void *append_cuad(CODE* C, CUAD *cd); //Agrega una cuadrupla al final de la lista
